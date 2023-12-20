16618db59e109a1ea9ef31ee03eb54e535f18d60
Nehemiah60 <nehemiabosire41@gmail.com>
2023-12-20 21:31:53 +0300

    Added post update

--- flask_community/views.py
+++ flask_community/views.py
@@ -1,8 +1,8 @@
 
 from datetime import datetime
 from flask_community import app, bcrypt, db, login_manager
-from flask import render_template, url_for, redirect, flash
-from flask_community.forms import RegisterUserForm, LoginForm
+from flask import render_template, url_for, redirect, flash, abort, request
+from flask_community.forms import RegisterUserForm, LoginForm, PostForm,UpdatePostForm
 from flask_community.models import User, Post
 from flask_login import login_user, login_required, current_user, logout_user
 
@@ -10,9 +10,10 @@ from flask_login import login_user, login_required, current_user, logout_user
 @app.route('/home')
 @login_required
 def home():
+    form = UpdatePostForm()
     posts = Post.query.all()
     now = datetime.utcnow()
-    return render_template('home.html', posts=posts, now=now)
+    return render_template('home.html', posts=posts, now=now, form=form)
 
 @app.route('/register', methods=['POST', 'GET'])
 def register_user():
@@ -55,6 +56,63 @@ def login():
 def logout():
     logout_user()
     return redirect(url_for('login'))
+
+@app.route('/post/new', methods=['POST', 'GET'])
+@login_required
+def new_post():
+    form = PostForm()
+    if form.validate_on_submit():
+        try:
+            post = Post( title = form.title.data,
+                         content = form.content.data,
+                         author  = current_user
+            )
+            db.session.add(post)
+            db.session.commit()
+            flash('Post created successfully', 'success')
+            return redirect(url_for('home'))
+        except:
+            flash('Fail!')
+    return render_template('create_post.html', form=form)
+
+#Delete Post...A user is the only one cable to delete a post
+@app.route('/delete_post/<int:post_id>', methods=['GET', 'POST'])
+def delete_post(post_id):
+    try: 
+        post = Post.query.get_or_404(post_id)
+        if post.author != current_user:
+            abort(403)
+        db.session.delete(post)
+        db.session.commit()
+        return redirect(url_for('home'))
+    except:
+        db.session.rollback
+    finally:
+        db.session.close()
+    return render_template('home.html', post=post)
+
+#Update Post
+@app.route('/update_post/<int:post_id>', methods=['GET', 'POST'])
+@login_required
+def update_post(post_id):
+    form = UpdatePostForm()
+    post = Post.query.get_or_404(post_id)
+    if post.author != current_user:
+        abort(403)
+    if form.validate_on_submit():
+        try:
+            post.title   = form.title.data
+            post.content = form.content.data
+            db.session.commit()
+            flash('Your Post Updated Successfully', 'success')
+            return redirect(url_for('home'))
+        except:
+            db.session.rollback()
+    elif request.method == 'GET':
+        form.title.data = post.title
+        form.content.data = post.content
+    return render_template('home.html', form=form)
+
 
 @app.errorhandler(401)
 def unauthorized(error):
